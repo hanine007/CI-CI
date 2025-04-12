84ca1ffe9e8bedfbf93e68daca84da341e5c28b6
Hanine <91432774+hanine007@users.noreply.github.com>
2025-04-12 11:33:56 +0000

    test

--- test/api.test.js
+++ test/api.test.js
@@ -3,7 +3,7 @@ const app = require('../src/index.js');
 
 
 describe('/', () => {
-  it('should return 200 OK', async () => {
+  it('should return 200 ', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
   });
